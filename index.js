08b63dca1b66cbd652026a62f961eef1216eab66
sam <sam@freighttrust.com>
2020-08-02 01:24:38 -0700

    feat(release): v1.0.0
    release of package

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright  FreightTrust and Clearing Corporation
+ *
+ * This source code is licensed under the Apache-2.0 license
+ */
+
+'use strict';
+
+module.exports = {
+  jsxBracketSameLine: true,
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'es5',
+  proseWrap: 'always',
+};
