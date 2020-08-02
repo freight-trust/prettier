1b9ad61735ddc74e78e982ecc8246cbdaa6fdd15
Restyled.io <commits@restyled.io>
2020-08-02 12:17:47 +0000

    Restyled by shfmt

--- bin/finish-upgrade.sh
+++ bin/finish-upgrade.sh
@@ -3,7 +3,7 @@ set -e
 script_dir=$(dirname "$0")
 GIT_COMMITTER_NAME="auto-prettier" GIT_COMMITTER_EMAIL="auto-prettierr@freighttrust.com" \
   git commit --author="auto-prettier <auto-prettier@freighttrust.com>" \
-             --all --message="chore: automated prettier" --no-verify
+  --all --message="chore: automated prettier" --no-verify
 git tag auto/post-prettier
 git push origin auto/pre-prettier auto/post-prettier master
 "$script_dir"/get-branches.sh | xargs -n 100 "$script_dir"/upgrade-branches.sh
