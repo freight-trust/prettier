d74a1e1d333107aa14649deedc7ac4f3f1df78f1
Restyled.io <commits@restyled.io>
2020-08-02 12:17:47 +0000

    Restyled by shellharden

--- bin/finish-upgrade.sh
+++ bin/finish-upgrade.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
-script_dir=$(dirname $0)
+script_dir=$(dirname "$0")
 GIT_COMMITTER_NAME="auto-prettier" GIT_COMMITTER_EMAIL="auto-prettierr@freighttrust.com" \
   git commit --author="auto-prettier <auto-prettier@freighttrust.com>" \
              --all --message="chore: automated prettier" --no-verify
 git tag auto/post-prettier
 git push origin auto/pre-prettier auto/post-prettier master
-$script_dir/get-branches.sh | xargs -n 100 $script_dir/upgrade-branches.sh
+"$script_dir"/get-branches.sh | xargs -n 100 "$script_dir"/upgrade-branches.sh
--- bin/upgrade-branches.sh
+++ bin/upgrade-branches.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
-script_dir=$(dirname $0)
+script_dir=$(dirname "$0")
 current_branch=$(git symbolic-ref --short HEAD)
-for branch in $@; do
-  git checkout $branch
-  git pull origin $branch
-  $script_dir/upgrade-branch.sh || continue
-  git push -f origin $branch-auto-prettier-backup $branch || true
+for branch in "$@"; do
+  git checkout "$branch"
+  git pull origin "$branch"
+  "$script_dir"/upgrade-branch.sh || continue
+  git push -f origin "$branch"-auto-prettier-backup "$branch" || true
 done
-git checkout $current_branch
+git checkout "$current_branch"
