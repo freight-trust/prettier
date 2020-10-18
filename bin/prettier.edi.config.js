73704d62a7a0d5f48c27424220b300b09d2f2721
sam bacha <sam@freighttrust.com>
2020-10-17 18:27:15 -0700

    fix: conformance issue

--- bin/prettier.edi.config.js
+++ bin/prettier.edi.config.js
@@ -1,9 +1,14 @@
+// XML/EDI Prettier Configuration for Data Interchange specific Applications
+// The aim of this module is to perserve the data strcuture in such a way that
+// there will be no issue parsing it. While still also maintaining some enforcable guidelines
+// SPDX-License-Identifier: ISC
+
 module.exports = {
     tabWidth: 2,
-    printWidth: 80,
+    printWidth: 140,
     proseWrap: 'preserve',
     semi: false,
-    trailingComma: 'es5',
+    trailingComma: 'none',
     singleQuote: false,
     arrowParens: 'avoid',
     overrides: [
@@ -14,6 +19,7 @@ module.exports = {
             quoteProps: 'preserve',
             singleQuote: false,
             trailingComma: 'all',
+            xmlWhitespaceSensitivity: 'strict' // Whitespaces are considered sensitive in all elements
         },
       },
     ],
