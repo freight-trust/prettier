7530a7e8b1e168af41c5e3d8f61a44a9f8777bd4
sam bacha <sam@freighttrust.com>
2020-10-31 23:21:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-/**
- * SPDX-License-Identifier: Apache-2.0
- * Copyright  FreightTrust and Clearing Corporation
- *
- * This source code is licensed under the Apache-2.0 license
- */
-
+/* eslint-disable */
+//
+// SPDX-License-Identifier: Apache-2.0
+// Copyright  FreightTrust and Clearing Corporation
+// 
+// This source code is licensed under the Apache-2.0 license
+// prettier.config.js or .prettierrc.js
 "use strict";
 
 module.exports = {
   jsxBracketSameLine: true,
-  printWidth: 80,
+  jsxSingleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
   singleQuote: true,
-  trailingComma: "es5",
-  proseWrap: "always",
+  arrowParens: 'always',
+  quoteProps: 'consistent',
+  bracketSpacing: true,
+  trailingComma: 'es5',
+  proseWrap: 'never',
+  endOfLine: 'lf',
 };
